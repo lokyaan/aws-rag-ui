aa84b054613a9cf73b864411eca982fa94f0f652
lokyaan <kotilokyaan@gmail.com>
2025-11-17 02:06:28 +0530

    adding url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE =
-  import.meta.env.VITE_API_BASE ??
-  "https://tefbdszk4xdna5tchrhck43u440vndqk.lambda-url.us-east-1.on.aws/";
+export const API_BASE = "https://tefbdszk4xdna5tchrhck43u440vndqk.lambda-url.us-east-1.on.aws";
+export const LS_KEY   = "siliconlabs_chat_history_v1";
+export const BRAND    = "SiliconLabs";
