773e1d3160165dde1c5f502caf5dcfead646e3a8
lokyaan <kotilokyaan@gmail.com>
2026-01-03 20:10:41 +0530

    CDK changes

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const API_BASE = "https://wml1a8nafj.execute-api.us-east-1.amazonaws.com";
 export const LS_KEY   = "siliconlabs_chat_history_v1";
-export const BRAND    = "CM.com";
+export const BRAND    = "CDK Global";
